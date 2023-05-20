6e728a1257c8d8d5365fbcccfb9882b96f18f72a
biliblihuorong <40906916+biliblihuorong@users.noreply.github.com>
2023-05-20 14:29:38 +0800

    新增接口

---
+++ controller/src/main/java/top/asshell/controller/CommonController.java
@@ -0,0 +1,53 @@
+package top.asshell.controller;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+import top.asshell.common.result.Result;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/common/")
+@Api("通用接口")
+public class CommonController {
+
+
+    @Value("${upload.imgPath}")
+    private String basePath;
+
+
+    @ApiOperation("通用图片上传接口")
+    @PutMapping("/uploadImg")
+    public Result uploadImg(MultipartFile multipartFile) {
+        //原始文件名
+        String originalFilename = multipartFile.getOriginalFilename();//abc.jpg
+        String suffix = originalFilename.substring(originalFilename.lastIndexOf("."));
+
+        //使用UUID重新生成文件名，防止文件名称重复造成文件覆盖
+        String fileName = UUID.randomUUID().toString() + suffix;//dfsdfdfd.jpg
+
+        //创建一个目录对象
+        File dir = new File(basePath);
+        //判断当前目录是否存在
+        if (!dir.exists()) {
+            //目录不存在，需要创建
+            dir.mkdirs();
+        }
+
+        try {
+            //将临时文件转存到指定位置
+            multipartFile.transferTo(new File(basePath + fileName));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return Result.success(fileName);
+    }
+}
+
---
+++ controller/src/main/java/top/asshell/controller/ProjectMaintenanceController.java
@@ -0,0 +1,40 @@
+package top.asshell.controller;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiModelProperty;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import top.asshell.common.result.Result;
+import top.asshell.dto.ProjectMaintenanceDTO;
+import top.asshell.pojo.ProjectMaintenance;
+import top.asshell.service.impl.ProjectMaintenanceServiceImpl;
+
+@RestController
+@RequestMapping("/projectMaintenance")
+@Api("项目维护接口")
+public class ProjectMaintenanceController {
+    @Autowired
+    private ProjectMaintenanceServiceImpl service;
+    @ApiOperation("查询")
+    @GetMapping("/getList")
+    public Result getList(ProjectMaintenanceDTO maintenanceDTO){
+        return service.getList(maintenanceDTO);
+    }
+    @ApiOperation("新增")
+    @PostMapping("/add")
+    public Result add(ProjectMaintenance projectMaintenance){
+        return service.add(projectMaintenance);
+    }
+    @ApiOperation("更新")
+    @PutMapping("/update")
+    public Result update(ProjectMaintenance projectMaintenance){
+        return service.update(projectMaintenance);
+    }
+    @ApiOperation("删除")
+    @DeleteMapping("/delete")
+    public Result delete(Integer ids []){
+        return service.delete(ids);
+    }
+
+}
